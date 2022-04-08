d2af7fda6ab45350e74296ecdce556ce6104e18b
HarrynVega <tomasharryn@outlook.com>
2022-04-08 00:10:30 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'php/main/head.php';
+include 'php/main/body.php';
+include 'php/main/foot.php';
+include 'php/portfolio/portfolio.php';
+include 'php/skillset/skillset.php';
+include 'php/experience/experience.php';
+?>
---
+++ php/main/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <!-- Contenido de mi pagina -->
+  <head>
+      <title>
+          Harryn Vega - PortFolio
+      </title>
+      <script type="text/javascript" src="libs/js/jquery-3.6.0.min.js"></script>
+      <link rel="stylesheet" href="libs/css/main.css">
+      <script type="text/javascript" src="libs/bootstrap/js/bootstrap.js"></script>
+   </head>
+ 
